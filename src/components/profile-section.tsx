ea33df89635cca20737fb3dc43c5d95935b7c912
Xuan Nguyen <121403665+xuannguyen04@users.noreply.github.com>
2026-01-10 16:19:27 -0600

    Update cv to resume

--- src/components/profile-section.tsx
+++ src/components/profile-section.tsx
@@ -82,7 +82,7 @@ export function ProfileSection({ aboutMe }: ProfileSectionProps) {
                 size={12}
                 className="group-hover:translate-x-0.5 group-hover:-translate-y-0.5 transition-transform duration-300"
               />
-              <span className="tracking-wider uppercase">CV</span>
+              <span className="tracking-wider uppercase">RESUME</span>
             </a>
           )}
         </div>
