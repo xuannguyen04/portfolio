991ff358fbd4af6d768140beac4bc23ceb03cc8e
Xuan Nguyen <121403665+xuannguyen04@users.noreply.github.com>
2026-01-10 15:25:00 -0600

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -19,19 +19,18 @@ export interface AboutMe {
 
 export const aboutMe: AboutMe = {
   name: "Xuan Nguyen",
-  title: "Ph.D. Candidate",
   institution: "University of Texas at Dallas",
   // Note that links work in the description
   description:
     "I am a graduate student at the University of Texas at Dallas pursuing a Master’s degree in Business Analytics and Artificial Intelligence, with a strong foundation in Logistics and Supply Chain Management. I am actively seeking a full-time position where I can apply advanced data analytics, machine learning, and operational expertise to drive data-informed business decisions.",
-  email: "______@stanford.edu",
+  email: "khanhxuannguyen04@gmail.com",
   imageUrl:
-    "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://snipboard.io/GO0Upm.jpg",
   githubUsername: "xuannguyen04",
   linkedinUsername: "xuannguyen411",
   blogUrl: "https://",
   cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+  institutionUrl: "https://www.utdallas.edu/",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
