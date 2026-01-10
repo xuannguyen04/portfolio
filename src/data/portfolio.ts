59daf59f4de38966bdaa7836ef95e4b39d3ee63e
Xuan Nguyen <121403665+xuannguyen04@users.noreply.github.com>
2026-01-10 16:55:32 -0600

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -16,7 +16,7 @@ export const portfolioData: Portfolio[] = [
     technologies: ["R", "Decision Tree", "Logistics Regression"],
     projectUrl: "https://cometmail-my.sharepoint.com/:p:/g/personal/chk210000_utdallas_edu/IQD5tpK1bT9jQbt31yhTqh_hAexgt5z92Mw_-5aM9skg1Os?e=0D5MEy",
     imageUrl:
-      "https://imgur.com/a/POfYArp",
+      "https://img.freepik.com/free-photo/world-smile-day-emojis-arrangement_23-2149024492.jpg?semt=ais_hybrid&w=740&q=80",
     codeUrl: "https://github.com/xuannguyen04/what-drives-happiness/blob/main/Happiness_RNotebook(v2).Rmd",
   },
 ];
