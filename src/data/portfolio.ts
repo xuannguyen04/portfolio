2c8bb84894484c7fbdea10294aee342f5eef45c9
Xuan Nguyen <121403665+xuannguyen04@users.noreply.github.com>
2026-01-10 16:36:36 -0600

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -10,13 +10,13 @@ export interface Portfolio {
 export const portfolioData: Portfolio[] = [
   // Example entry
   {
-    title: "Causal Discovery Framework",
+    title: "What Drives Happiness? ",
     description:
       "A framework for discovering causal relationships in high-dimensional time series data using state-of-the-art machine learning techniques.",
-    technologies: ["Python", "PyTorch", "React"],
-    projectUrl: "https://project-demo.com",
+    technologies: ["R", "Decision Tree", "Logistics Regression"],
+    projectUrl: "https://cometmail-my.sharepoint.com/:p:/g/personal/chk210000_utdallas_edu/IQD5tpK1bT9jQbt31yhTqh_hAexgt5z92Mw_-5aM9skg1Os?e=0D5MEy",
     imageUrl:
-      "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    codeUrl: "https://github.com/username/project",
+      "https://drive.google.com/file/d/1KxtNzzjQVLXYZwf-hzyL0uXKVYWYO0L2/view?usp=sharing",
+    codeUrl: "https://github.com/xuannguyen04/what-drives-happiness",
   },
 ];
