b1b257d2a71f6dc1e0a79291f38a32506d042124
Xuan Nguyen <121403665+xuannguyen04@users.noreply.github.com>
2026-01-11 01:07:17 -0600

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -19,4 +19,34 @@ export const portfolioData: Portfolio[] = [
       "https://img.freepik.com/free-photo/world-smile-day-emojis-arrangement_23-2149024492.jpg?semt=ais_hybrid&w=740&q=80",
     codeUrl: "https://github.com/xuannguyen04/what-drives-happiness/blob/main/Happiness_RNotebook(v2).Rmd",
   },
+  {
+    title: "Warehouse and Supplier Order Management System",
+    description:
+      "Applied logistic regression and decision tree models in R to analyze the World Happiness dataset, identifying key determinants of global happiness including healthy life expectancy and social support",
+    technologies: ["SQL", "ERD"],
+    projectUrl: "https://cometmail-my.sharepoint.com/:p:/g/personal/xdn240000_utdallas_edu/IQAUarMSgdcRTqXCEu7qntnDAfuhIeolM7VPdJQLikJt4rY?e=8qAJVh",
+    imageUrl:
+      "https://media.istockphoto.com/id/1138429558/photo/rows-of-shelves.jpg?s=612x612&w=0&k=20&c=0E4uvaa-THb-Wj-QZKpUSPgwjfIMFW3vH7NRx1iWMIc=",
+    codeUrl: "https://github.com/xuannguyen04/supplier-order-database-management-",
+  },
+  {
+    title: "2.	Warehouse and Supplier Order Management System",
+    description:
+      "Applied logistic regression and decision tree models in R to analyze the World Happiness dataset, identifying key determinants of global happiness including healthy life expectancy and social support",
+    technologies: ["R", "Decision Tree", "Logistics Regression"],
+    projectUrl: "https://cometmail-my.sharepoint.com/:p:/g/personal/chk210000_utdallas_edu/IQD5tpK1bT9jQbt31yhTqh_hAexgt5z92Mw_-5aM9skg1Os?e=0D5MEy",
+    imageUrl:
+      "https://img.freepik.com/free-photo/world-smile-day-emojis-arrangement_23-2149024492.jpg?semt=ais_hybrid&w=740&q=80",
+    codeUrl: "https://github.com/xuannguyen04/what-drives-happiness/blob/main/Happiness_RNotebook(v2).Rmd",
+  },
+  {
+    title: "What Drives Happiness? ",
+    description:
+      "Applied logistic regression and decision tree models in R to analyze the World Happiness dataset, identifying key determinants of global happiness including healthy life expectancy and social support",
+    technologies: ["R", "Decision Tree", "Logistics Regression"],
+    projectUrl: "https://cometmail-my.sharepoint.com/:p:/g/personal/chk210000_utdallas_edu/IQD5tpK1bT9jQbt31yhTqh_hAexgt5z92Mw_-5aM9skg1Os?e=0D5MEy",
+    imageUrl:
+      "https://img.freepik.com/free-photo/world-smile-day-emojis-arrangement_23-2149024492.jpg?semt=ais_hybrid&w=740&q=80",
+    codeUrl: "https://github.com/xuannguyen04/what-drives-happiness/blob/main/Happiness_RNotebook(v2).Rmd",
+  }
 ];
