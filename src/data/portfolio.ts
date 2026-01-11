3e9fdc3876e64fb30972ce2fdfe59877486aba9c
Xuan Nguyen <121403665+xuannguyen04@users.noreply.github.com>
2026-01-11 13:57:09 -0600

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -32,12 +32,13 @@ export const portfolioData: Portfolio[] = [
   {
     title: "NBA Prediction",
     description:
-      "Applied logistic regression and decision tree models in R to analyze the World Happiness dataset, identifying key determinants of global happiness including healthy life expectancy and social support",
-    technologies: ["Python", "Decision Tree", "Logistics Regression"],
-    projectUrl: "https://cometmail-my.sharepoint.com/:p:/g/personal/chk210000_utdallas_edu/IQD5tpK1bT9jQbt31yhTqh_hAexgt5z92Mw_-5aM9skg1Os?e=0D5MEy",
+      "Analyzed 16 years of historical NBA data, finding out shooting accuracy and rebounding percentage as critical performance indicators, which correlated with a 15% increase in home team wins when optimized, resulting in higher fan engagement and increased merchandise sales
+",
+    technologies: ["Python", "Decision Tree", "Logistics Regression", "Linear SVM"],
+    projectUrl: "https://drive.google.com/file/d/1LM8meHxFdcSC-ROWCf_S8zGqpAhPFXug/view?usp=sharing",
     imageUrl:
-      "https://img.freepik.com/free-photo/world-smile-day-emojis-arrangement_23-2149024492.jpg?semt=ais_hybrid&w=740&q=80",
-    codeUrl: "https://github.com/xuannguyen04/what-drives-happiness/blob/main/Happiness_RNotebook(v2).Rmd",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL9aJ8TpeTtOTScedTXqJEdGE-Of-Xyic7hw&s",
+    codeUrl: "https://github.com/xuannguyen04/nba-prediction/blob/main/NBA_Prediction_Group2_BUAN6341.ipynb",
   },
   {
     title: "What Drives Happiness? ",
